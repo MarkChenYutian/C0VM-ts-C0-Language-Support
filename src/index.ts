1d514dd9e735e34e8f2c1761753ca5bb7b64f977
Yutian Chen <yutianch@andrew.cmu.edu>
2022-08-09 19:40:45 -0400

    :sparkles: buggy version of c0 syntax parser

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,86 @@
 import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
+import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside} from "@codemirror/language"
 import {styleTags, tags as t} from "@lezer/highlight"
 
 export const C0Language = LRLanguage.define({
   parser: parser.configure({
     props: [
-      indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
-      }),
       foldNodeProp.add({
-        Application: foldInside
+        Scope: foldInside
+      }),
+      indentNodeProp.add({
+        Scope: context => context.baseIndent + context.unit
       }),
       styleTags({
         Identifier: t.variableName,
+        Type: t.typeName,
+        DeclareType: t.typeName,
+
         Boolean: t.bool,
         String: t.string,
-        LineComment: t.lineComment,
-        "( )": t.paren
-      })
+        Number: t.number,
+        Char: t.character,
+
+        LineComment: t.comment,
+        BlockComment: t.comment,
+
+        "return": t.controlKeyword,
+        "if": t.controlKeyword,
+        "else": t.controlKeyword,
+        "while": t.controlKeyword,
+        "for": t.controlKeyword,
+        "alloc": t.keyword,
+        "alloc_array": t.keyword,
+        "#use": t.keyword,
+
+        "assert": t.keyword,
+        "loop_invariant": t.keyword,
+        "requires": t.keyword,
+        "ensures": t.keyword,
+
+        ContractBlock: t.annotation,
+        ContractLine: t.annotation,
+
+        "(": t.paren,
+        ")": t.paren,
+        "[": t.paren,
+        "]": t.paren,
+
+        // "+": t.arithmeticOperator,
+        // "-": t.arithmeticOperator,
+        // "*": t.arithmeticOperator,
+        // "/": t.arithmeticOperator,
+        // "%": t.arithmeticOperator,
+        // "+=": t.arithmeticOperator,
+        // "-=": t.arithmeticOperator,
+        // "*=": t.arithmeticOperator,
+        // "/=": t.arithmeticOperator,
+        // "%=": t.arithmeticOperator,
+        // "++": t.arithmeticOperator,
+        // "--": t.arithmeticOperator,
+
+        // "~": t.bitwiseOperator,
+        // "^": t.bitwiseOperator,
+        // "|": t.bitwiseOperator,
+        // "&": t.bitwiseOperator,
+        // "<<":t.bitwiseOperator,
+        // ">>":t.bitwiseOperator,
+
+        // "!": t.logicOperator,
+        // "&&": t.logicOperator,
+        // "||": t.logicOperator,
+        // "==": t.logicOperator,
+        // "!=": t.logicOperator,
+
+        ">=": t.compareOperator,
+        "<=": t.compareOperator,
+        ">": t.compareOperator,
+        "<": t.compareOperator,
+
+        "->": t.derefOperator,
+      }),
     ]
-  }),
-  languageData: {
-    commentTokens: {line: ";"}
-  }
+  })
 })
 
 export function C0() {
